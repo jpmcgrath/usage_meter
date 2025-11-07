c8cb2e0cc9ada1d580c794c714f35957f972f27c
James P. McGrath <james@shippit.com>
2025-11-08 06:52:54 +1000

    Switch to using gemfiles
    Using gemfiles for all the dependencies will allow us to have more control
    over sets of depdendencies that we want to test, not just rails version.
    
    Change migration version down to 8.0, so that it supports both 8.0 and 8.1.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_07_045726) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_07_045726) do
   create_table "usage_meter_customers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "external_identifier"
