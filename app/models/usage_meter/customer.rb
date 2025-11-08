80c5cf1ef1100c90da6a1eb8d3d0bb2db9374827
James P. McGrath <james@shippit.com>
2025-11-09 07:33:32 +1000

    Give an outline of the Customer model

--- app/models/usage_meter/customer.rb
+++ app/models/usage_meter/customer.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module UsageMeter
+  # UsageMeter Customer model
+  # A customer in an enttity who's usage is being metered. This is typically a Tenant
+  # Organization, or User.
+  #
+  # UseageMeter tracks it's own customers to ensure consistency and integrity of usage data.
+  # Each customer will have a unique composite key of external_type and external_identifier.
+  # Usage data is then associated with these customers, with foreign keys referencing the
+  # to ensure referential integrity.
+  #
+  # Once created, records are readonly by design to prevent tampering with historical usage data.
   class Customer < ApplicationRecord
     validates :external_type, presence: true
     validates :external_identifier, presence: true, uniqueness: { scope: :external_type }
