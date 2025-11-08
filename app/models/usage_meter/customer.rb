bde77767d8c2738531982a0541fe7888322d836f
James P. McGrath <james@shippit.com>
2025-11-08 14:30:55 +1000

    Make Customer immutable after creation

--- app/models/usage_meter/customer.rb
+++ app/models/usage_meter/customer.rb
@@ -1,4 +1,12 @@
 module UsageMeter
   class Customer < ApplicationRecord
+    validates :external_type, presence: true
+    validates :external_identifier, presence: true, uniqueness: { scope: :external_type }
+
+    attr_readonly :external_type, :external_identifier, :created_at, :updated_at
+
+    def readonly?
+      persisted?
+    end
   end
 end
--- test/models/usage_meter/customer_test.rb
+++ test/models/usage_meter/customer_test.rb
@@ -2,8 +2,22 @@ require "test_helper"
 
 module UsageMeter
   class CustomerTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "allows creation but is readonly after save" do
+      # creation should be allowed
+      assert_difference "UsageMeter::Customer.count", 1 do
+        UsageMeter::Customer.create!(external_type: "test_type", external_identifier: "test_id")
+      end
+
+      customer = UsageMeter::Customer.find_by(external_type: "test_type", external_identifier: "test_id")
+      assert customer.persisted?
+
+      # model defines readonly? to be true once persisted
+      assert customer.readonly?
+
+      # attempting to update a persisted record should raise ActiveRecord::ReadOnlyRecord
+      assert_raises(ActiveRecord::ReadOnlyRecord) do
+        customer.update!(human_description: "new description")
+      end
+    end
   end
 end
