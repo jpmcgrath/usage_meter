773e6f48199e1f0b166ac396053076fce3c9d22f
James P. McGrath <james@shippit.com>
2025-11-13 22:21:26 +1000

    manually edit to allow compatible with all rails versions in matrix

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_11_204821) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_11_204821) do
   create_table "usage_meter_customers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "external_identifier"
