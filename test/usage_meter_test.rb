7680c1129614e9b284764bbd4a325e34bc1af10d
James P. McGrath <james@shippit.com>
2025-11-13 20:17:21 +1000

    Remove minitest

--- test/usage_meter_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class UsageMeterTest < ActiveSupport::TestCase
-  test 'it has a version number' do
-    assert UsageMeter::VERSION
-  end
-end
