72e499ff69489690da0a043fd2c303ed7f986eb1
James P. McGrath <james@shippit.com>
2025-11-18 22:07:30 +1000

    Add associations and validations for EventLog

--- app/models/usage_meter/event_log.rb
+++ app/models/usage_meter/event_log.rb
@@ -2,6 +2,18 @@ module UsageMeter
   class EventLog < ApplicationRecord
     belongs_to :customer
     belongs_to :event_type
-    belongs_to :aggregate
+    # belongs_to :aggregate
+
+    validates :publishable_id, presence: true, uniqueness: true
+    validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validates :version, presence: true
+    validates :event_type, presence: true
+    validates :customer, presence: true
+
+    attr_readonly :publishable_id, :customer_id, :event_type_id, :created_at, :updated_at
+
+    def readonly?
+      persisted?
+    end
   end
 end
--- spec/models/usage_meter/event_log_spec.rb
+++ spec/models/usage_meter/event_log_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 module UsageMeter
-  RSpec.describe EventLog, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.describe EventLog do
+
+    describe 'associations' do
+      it { is_expected.to belong_to(:customer).class_name('Customer') }
+      it { is_expected.to belong_to(:event_type).class_name('EventType') }
+    end
+
+    describe 'validations' do
+      it { is_expected.to validate_presence_of(:quantity) }
+      it { is_expected.to validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
+      it { is_expected.to validate_presence_of(:customer) }
+      it { is_expected.to validate_presence_of(:event_type) }
+    end
   end
 end
