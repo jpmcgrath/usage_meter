7e8b0c8cbc81fea6d63efdb487a9f5ef4947ebac
James P. McGrath <james@shippit.com>
2025-11-18 22:05:54 +1000

    Adjust migration for compatability with Rails and DBs
    Tweak migration so that it works with older versions
    of rails, and with mysql and sqlite.

--- db/migrate/20251118111110_create_usage_meter_event_logs.rb
+++ db/migrate/20251118111110_create_usage_meter_event_logs.rb
@@ -1,15 +1,25 @@
-class CreateUsageMeterEventLogs < ActiveRecord::Migration[8.1]
+class CreateUsageMeterEventLogs < ActiveRecord::Migration[7.0]
   def change
     create_table :usage_meter_event_logs do |t|
-      t.string :publishable_id
+      if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+        t.string :publishable_id, null: false, index: { unique: true }, collation: "utf8mb4_bin"
+      else
+        t.string :publishable_id, null: false, index: { unique: true }
+      end
       t.references :customer, null: false, foreign_key: true
       t.references :event_type, null: false, foreign_key: true
-      t.integer :quantity
-      t.json :extra_data
+      t.integer :quantity, default: 1, null: false
       t.string :version
-      t.references :aggregate, null: false, foreign_key: true
+      # t.references :aggregate, null: true, foreign_key: true
+
+      if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+        t.jsonb :extra_data
+      else
+        t.json :extra_data
+      end
 
       t.timestamps
     end
+    add_index :usage_meter_event_logs, %i[customer_id event_type_id]
   end
 end
